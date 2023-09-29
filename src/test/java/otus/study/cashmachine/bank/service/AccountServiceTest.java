5a38348d312fb072d45a4d110d80f05b2babeb75
Ivan Haydarov <navi1435@gmail.com>
2023-09-29 17:41:41 +0700

    Revert "correct files added"
    This reverts commit a3a99b17b01ddffc5dc3448e8f8ef3fb9a4b416b.

--- src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
+++ src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
@@ -1,108 +1,40 @@
 package otus.study.cashmachine.bank.service;
 
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import otus.study.cashmachine.bank.dao.AccountDao;
-import otus.study.cashmachine.bank.data.Account;
 import otus.study.cashmachine.bank.service.impl.AccountServiceImpl;
-import java.math.BigDecimal;
-
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
 
 
 public class AccountServiceTest {
 
+    AccountDao accountDao;
 
-    static AccountDao accountDao;
-
-    static AccountService accountService;
-
-
-    @BeforeEach
-    void init(){
-        accountDao = mock(AccountDao.class);
-        accountService = new AccountServiceImpl(accountDao);
-
-    }
+    AccountServiceImpl accountServiceImpl;
 
     @Test
     void createAccountMock() {
-//        @TODO test account creation with mock and ArgumentMatcher
-        when(accountDao.saveAccount(any(Account.class))).thenReturn(new Account(0, new BigDecimal(1000)));
-        assertEquals(accountService.createAccount(new BigDecimal(1000)).getAmount(),
-                new BigDecimal(1000));
+// @TODO test account creation with mock and ArgumentMatcher
     }
-
-
 
     @Test
     void createAccountCaptor() {
 //  @TODO test account creation with ArgumentCaptor
-
-        ArgumentCaptor<Account> captor = ArgumentCaptor.forClass(Account.class);
-        when(accountDao.saveAccount(captor.capture())).thenReturn(new Account(0, new BigDecimal(2)));
-        assertEquals(accountService.createAccount(new BigDecimal(2)), captor.getValue());
-
-
     }
 
     @Test
     void addSum() {
-        Account actualAccount = new Account(1, new BigDecimal(2));
-        ArgumentCaptor<Long> longArgumentCaptor = ArgumentCaptor.forClass(Long.class);
-        when(accountDao.getAccount(longArgumentCaptor.capture())).thenReturn(actualAccount);
-        accountService.putMoney(1L, new BigDecimal(2));
-        Account expectedAccount = new Account(1L, new BigDecimal(4));
-        verify(accountDao, only()).getAccount(anyLong());
-        assertEquals(expectedAccount, actualAccount);
-        assertEquals(1, longArgumentCaptor.getValue().longValue());
-
-
     }
 
     @Test
     void getSum() {
-        Account accountBeforeGetSum = new Account(1, new BigDecimal(2));
-        ArgumentCaptor<Long> longArgumentCaptor = ArgumentCaptor.forClass(Long.class);
-        when(accountDao.getAccount(longArgumentCaptor.capture())).thenReturn(accountBeforeGetSum);
-        accountService.getMoney(1L, new BigDecimal(2));
-        Account accountAfterGetSum = new Account(longArgumentCaptor.getValue(), new BigDecimal(0));
-        verify(accountDao, only()).getAccount(longArgumentCaptor.getValue());
-        assertEquals(accountBeforeGetSum, accountAfterGetSum);
-    }
-
-    @Test
-    void getSumException(){
-        Account account = new Account(1, new BigDecimal(10));
-        when(accountDao.getAccount(1L)).thenReturn(account);
-        try{
-            accountService.getMoney(1L, new BigDecimal(100_000));
-        } catch (IllegalArgumentException e){
-            assertEquals("Not enough money", e.getMessage());
-            return;
-        }
-        fail();
-
     }
 
     @Test
     void getAccount() {
-        Account expectedAccount = new Account(1L, new BigDecimal(1));
-        when(accountDao.getAccount(1L)).thenReturn(expectedAccount);
-        assertEquals(expectedAccount, accountService.getAccount(1L));
-
     }
 
     @Test
     void checkBalance() {
-        BigDecimal expectedBalance = new BigDecimal(1_000_000);
-        when(accountDao.getAccount(1L)).thenReturn(new Account(1L, expectedBalance));
-        assertEquals(expectedBalance, accountService.checkBalance(1L));
     }
 }
